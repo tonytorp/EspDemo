bf7c3112d966cdf362b7804642909d1882fed0dc
tonytorp <tony.torp@tuni.fi>
2025-02-17 14:17:45 +0200

    Ensimmainen GUI -versio

--- app/src/main/java/com/example/demosovellus/MainActivity.kt
+++ app/src/main/java/com/example/demosovellus/MainActivity.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.demosovellus.ui.theme.DemosovellusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,61 +38,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun EspControllerScreen(){
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        Text( text = "Hello, World!", fontSize = 40.sp )
-        Button( onClick = { /* Do something! */ } ) {
-            Text("Click me!")
-        }
-        Button( onClick = { /* Do something! */ } ) {
-            Text("Click me!")
-        }
-        Button( onClick = { /* Do something! */ } ) {
-            Text("Click me!")
-        }
-        Button( onClick = { /* Do something! */ } ) {
-            Text("Click me!")
-        }
-        Button( onClick = { /* Do something! */ } ) {
-            Text("Click me!")
-        }
-        Button( onClick = { /* Do something! */ } ) {
-            Text("Click me!")
-        }
+fun Header() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp)) // Pyöristetyt kulmat
+            .background(Color(0xFF64B5F6)) // Vaaleampi sininen väri
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Polhu",
+            fontSize = 50.sp,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+        )
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun EspControllerScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
     )
-}
+    {
+        Spacer(modifier = Modifier.height(20.dp))
+        Header()
+        Spacer(modifier = Modifier.height(20.dp))
+        Button( onClick = { /* Do something! */ } ) {
+            Text("Vapautus")
+        }
+        Button( onClick = { /* Do something! */ } ) {
+            Text("Eteen")
+        }
+        Button( onClick = { /* Do something! */ } ) {
+            Text("Taakse")
+        }
+        Button( onClick = { /* Do something! */ } ) {
+            Text("Kuormitus")
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DemosovellusTheme {
-        Greeting("Android")
     }
 }
